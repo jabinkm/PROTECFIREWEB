787ee39cee10d04edbe27a51123561d6ff13624e
jabinkm <36657358+jabinkm@users.noreply.github.com>
2026-07-04 11:24:53 +0400

    instagram is linked to the hero image

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -122,7 +122,13 @@ export function Hero() {
         </a>
         </motion.div>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <IconButton color="white" variant="outlined" size="sm" className="hover:text-white hover:border-red-500 hover:bg-black">
+
+          <a href="https://www.instagram.com/protecfireuae/">
+          <IconButton color="white" variant="outlined" size="sm" className="hover:text-blue-700 hover:border-red-500 hover:bg-white">
+            <i className="fab fa-instagram" />
+          </IconButton>
+          </a>
+          {/* <IconButton color="white" variant="outlined" size="sm" className="hover:text-white hover:border-red-500 hover:bg-black">
             <svg
               className="h-5 w-5"
               fill="currentColor"
@@ -131,7 +137,7 @@ export function Hero() {
             >
               <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
             </svg>
-          </IconButton>
+          </IconButton> */}
         </motion.div>
       </motion.div>
     </div>
